bd18d9cb8fb5568e6a0f960a691e0e67db74905b
rdunavant0 <rdunavant0@gmail.com>
2019-01-25 15:51:02 -0600

    message

---
+++ P1.py
@@ -0,0 +1,75 @@
+def main():
+def problem1():
+def problem2():
+def problem3():
+def problem4():
+
+class Dog:
+    def __init__(self, name = "", breed = "", color = "", gender = ""):
+        self.name = name
+        self.breed = breed
+        self.color = color
+        self.gender = gender
+    def printAll(self):
+        print(self.name, self.breed, self.color, self.gender)
+    def __str__(self):
+        return(self.name, self.breed, self.color, self.gender)
+def problem1():
+    newDog1 = Dog()
+    newDog2 = Dog(name)
+    newDog3 = Dog(name, "breed")
+    newDog4 = Dog(name, "breed", "color")
+    newDog5 = Dog(name, "breed", "color", "gender")
+    print(newDog1)
+    print(newDog2)
+    print(newDog3)
+    print(newDog4)
+    print(newDog5)
+    newDog1 = Dog(name, "breed", "color", "gender")
+    print(newDog1.printAll())
+
+def problem2():
+    userInput = ""
+    while(userInput != '='):
+        userInput = input("Enter number")
+        userInput2 = input("Enter another number")
+        if userInput == '='
+            print(=)
+
+def problem3():
+    Person1 = person
+    Person2 = person
+    Person3 = person
+    .change height and weight__(Person1)
+    .change height and weight__(Person2)
+    .change height and weight__(Person3)
+    Person1Weight/(Person1Height/Person1Height)) * 703
+    Person2Weight/(Person2Height/Person2Height)) * 703
+    Person3Weight/(Person3Height/Person3Height)) * 703
+
+class Product:
+        def __init__(self, name = "", price = "", quantity = ""):
+            self.name = name
+            self.price = price
+            self.quantity = quantity
+            printAll(self)
+            print(self.name, self.price, self.quantity)
+        def _str__(self):
+            return(self.name, self.price, self.quantity)
+def problem4():
+    def changeProduct():
+        if changeProduct(x):
+            .change(name)
+        if changeProduct (x,y):
+            .change(name and price)
+        if changeProduct(x,y,z):
+            .change(name and price and quantity)
+    newProduct1():
+    newProduct2(name):
+    newProduct3(name, "price")
+    newProduct4(name, "price", "quantity")
+    print(newProduct1)
+    print(newProduct2)
+    print(newProduct3)
+    print(newProduct4)
+    
